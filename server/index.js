f5c7504b947ba7aa2b015da4296483fefe148d8c
“” <xxx@xx>
2018-04-20 15:52:44 +0800

    commit

--- server/index.js
+++ server/index.js
@@ -29,5 +29,5 @@ if (config.dev) {
 app.use(nuxt.render)
 
 // Listen the server
-app.listen(port, '0.0.0.0')
+app.listen(port)
 console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
